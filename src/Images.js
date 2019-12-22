ff1b5721dce987d81606857afee0df9efc995e20
3113v3n <sydneyreezy@gmail.com>
2019-12-22 16:48:15 +0300

    ..

--- src/Images.js
+++ src/Images.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './Images.css';
-import "~slick-carousel/slick/slick.css";
-import "~slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
 
 const photos = [
